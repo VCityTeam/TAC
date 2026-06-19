9089ce60e7a66dcb03a1adef8542e2aa66c5ab65
AsriRida99 <asri.travaille@gmail.com>
2026-06-19 17:42:02 +0200

    Suppression submodule TAC cassé

---
+++ TAC-Backend/Services/Prompt engineering/config.py
@@ -0,0 +1,10 @@
+# URL for Ollama API
+OLLAMA_BASE_URL = "http://localhost:11434"
+OLLAMA_GENERATE_URL = f"{OLLAMA_BASE_URL}/api/v1/generate"
+
+# Path to the CSV file containing terms
+TERMES_PATH = "C:/Users/redaa/OneDrive/Desktop/TAC/Data/terms.csv" 
+
+# 
+MODEL_MISTRAL = "mistral"
+MODEL_LLAMA = "llama3.1:8b"
